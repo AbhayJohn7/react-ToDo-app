0fe04171979944f5e041fd90b7219edef66f24c9
5710 - Abhay John <5710abj@manorama.net>
2024-10-11 15:58:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, toggleTodo, deleteTodo } from './features/todoSlice';
+import './App.css';
+
+function App() {
+  const [toDo, setToDo] = useState('');
+  const dispatch = useDispatch();
+  const toDos = useSelector((state) => state.todos.todos);
+
+  const handleAddTodo = () => {
+    if (toDo.trim()) {
+      dispatch(addTodo(toDo));
+      setToDo('');
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <h2> ADD THINGS TODO </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="Let's do this!"
+        />
+        <i onClick={handleAddTodo} className="fas fa-plus"></i>
+      </div>
+
+      <div className="todos">
+        {toDos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="left">
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => dispatch(toggleTodo(todo.id))}
+              />
+              <p>{todo.text}</p>
+            </div>
+            <div className="right">
+              <i
+                onClick={() => dispatch(deleteTodo(todo.id))}
+                className="fas fa-times"
+              ></i>
+            </div>
+          </div>
+        ))}
+
+        <div className="checked-items">
+          {toDos
+            .filter((todo) => todo.status)
+            .map((todo) => (
+              <h3
+                key={todo.id}
+                style={{
+                  color: 'white',
+                  textDecoration: 'line-through',
+                  marginTop: '8px',
+                }}
+              >
+                {todo.text}
+              </h3>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
